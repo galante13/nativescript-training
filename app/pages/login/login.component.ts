0e048349667781a8dedcaaa979ec2d18fd24fce3
Gustaw Wypych <gustaw.wypych@gmail.com>
2018-01-07 15:53:07 +0100

    Adding background img for login page

--- app/pages/login/login.component.ts
+++ app/pages/login/login.component.ts
@@ -1,5 +1,7 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
+
+import { Page } from "ui/page";
 
 import { User } from "../../shared/user/user";
 import { UserService } from "../../shared/user/user.service";
@@ -10,15 +12,23 @@ import { UserService } from "../../shared/user/user.service";
     templateUrl: "./pages/login/login.html",
     styleUrls: ["pages/login/login-common.css", "pages/login/login.css"]
 })
-export class LoginComponent {
-
+export class LoginComponent implements OnInit {
     user: User;
     isLoggingIn = true;
 
-    constructor(private router: Router, private userService: UserService) {
+    constructor(
+        private router: Router,
+        private userService: UserService,
+        private page: Page) {
+            
         this.user = new User();
         this.user.email = "my.test.account@nativescript.org";
         this.user.password = "password";
+    }
+
+    ngOnInit(): void {
+        this.page.actionBarHidden = true;
+        this.page.backgroundImage = "res://bg_login";
     }
 
     submit() {
